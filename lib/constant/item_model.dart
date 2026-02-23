fffc23ed0d4371a655c1daa83da1782fecd4fc77
sumayacse00724105101110-pixel <sumaya.cse.00724105101110@aust.edu>
2026-02-23 12:28:34 +0600

    variable store

---
+++ lib/constant/item_model.dart
@@ -0,0 +1,15 @@
+class Item {
+  final String image;
+  final String name;
+  final String subname;
+  final double price;
+  final double rating;
+
+  Item({
+    required this.image,
+    required this.name,
+    required this.subname,
+    required this.price,
+    required this.rating,
+  });
+}
